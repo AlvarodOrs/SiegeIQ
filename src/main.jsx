3fa6373facf015414fa845a01ff2411fb7dc132d
Álvaro d'Ors Nestares <alvarodorsnestares@gmail.com>
2026-03-20 14:11:35 +0100

    Added Vercel tracking

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/globals.css'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 )
